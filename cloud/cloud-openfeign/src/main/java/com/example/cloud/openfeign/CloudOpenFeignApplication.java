1ff0a29814677fecd1be78445457bde985a02af9
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2023-03-20 11:48:22 +0100

    Add cloud-openfeign tests.

---
+++ cloud/cloud-openfeign/src/main/java/com/example/cloud/openfeign/CloudOpenFeignApplication.java
@@ -0,0 +1,51 @@
+package com.example.cloud.openfeign;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@EnableFeignClients(clients = TestServiceClient.class)
+public class CloudOpenFeignApplication implements CommandLineRunner {
+
+	private static final Log LOG = LogFactory.getLog(CloudOpenFeignApplication.class);
+
+	@Autowired
+	private TestServiceClient testServiceClient;
+
+	@GetMapping("/")
+	String test() {
+		return testServiceClient.test();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudOpenFeignApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		callTestService();
+	}
+
+	private void callTestService() {
+		if (LOG.isInfoEnabled()) {
+			LOG.info(testServiceClient.test());
+		}
+	}
+
+}
+
+@FeignClient("test-service")
+interface TestServiceClient {
+
+	@GetMapping("/")
+	String test();
+
+}
